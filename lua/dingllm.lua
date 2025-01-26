e4dd58a96303f794da5eed2190350ce1daafa248
Jesse Williams <jesse@negativespacesounds.com>
2025-01-27 09:41:40 +1100

    update: passing in llmfiles works, deepseek works

--- lua/dingllm.lua
+++ lua/dingllm.lua
@@ -1,5 +1,47 @@
 local M = {}
+
 local Job = require 'plenary.job'
+
+---@class LlmFileType
+---@field path string
+---@field contents string
+
+local LlmFile = {}
+LlmFile.__index = LlmFile
+
+---@param path string
+---@param contents string
+function LlmFile.new(path, contents)
+  local self = setmetatable({}, LlmFile)
+  self.path = path
+  self.contents = contents
+  return self
+end
+
+function LlmFile:formatted_contents()
+  return string.format("<contentblock file=%s>\n%s\n</contentblock>", self.path, self.contents)
+end
+
+---@return string[]
+local function get_llmfile_paths()
+  local contents = {}
+  local current_dir = vim.fn.getcwd()
+  local file_path = current_dir .. "/.llmfiles"
+
+  local file = io.open(file_path, "r")
+  if not file then
+    return {}
+  end
+
+  for line in file:lines() do
+    if line ~= "" then
+      table.insert(contents, line)
+    end
+  end
+
+  file:close()
+  return contents
+end
 
 local function get_api_key(name)
   return os.getenv(name)
@@ -45,7 +87,8 @@ function M.get_visual_selection()
       scol, ecol = ecol, scol
     end
     for i = srow, erow do
-      table.insert(lines, vim.api.nvim_buf_get_text(0, i - 1, math.min(scol - 1, ecol), i - 1, math.max(scol - 1, ecol), {})[1])
+      table.insert(lines,
+        vim.api.nvim_buf_get_text(0, i - 1, math.min(scol - 1, ecol), i - 1, math.max(scol - 1, ecol), {})[1])
     end
     return lines
   end
@@ -90,8 +133,29 @@ function M.make_openai_spec_curl_args(opts, prompt, system_prompt)
   return args
 end
 
+function M.make_deepseek_spec_curl_args(opts, prompt, system_prompt)
+  local url = opts.url
+  local api_key = opts.api_key_name and get_api_key(opts.api_key_name)
+  local data = {
+    messages = { { role = 'system', content = system_prompt }, { role = 'user', content = prompt } },
+    model = opts.model,
+    temperature = 0.0,
+    stream = true,
+  }
+  local args = { '-N', '-X', 'POST', '-H', 'Content-Type: application/json', '-d', vim.json.encode(data) }
+  if api_key then
+    table.insert(args, '-H')
+    table.insert(args, 'Authorization: Bearer ' .. api_key)
+  end
+  table.insert(args, url)
+  return args
+end
+
 function M.write_string_at_cursor(str)
   vim.schedule(function()
+    if str == vim.NIL then
+      return
+    end
     local current_window = vim.api.nvim_get_current_win()
     local cursor_position = vim.api.nvim_win_get_cursor(current_window)
     local row, col = cursor_position[1], cursor_position[2]
@@ -144,6 +208,9 @@ function M.handle_openai_spec_data(data_stream)
       if content then
         M.write_string_at_cursor(content)
       end
+    else
+      print("no choices found")
+      print(vim.inspect(json))
     end
   end
 end
@@ -151,10 +218,32 @@ end
 local group = vim.api.nvim_create_augroup('DING_LLM_AutoGroup', { clear = true })
 local active_job = nil
 
+---@param llm_paths string[]
+---@param system_prompt string
+local function system_prompt_with_files(llm_paths, system_prompt)
+  local files_string = "<system_prompt>\n" .. system_prompt .. "\n</system_prompt>"
+  for _, p in ipairs(llm_paths) do
+    local attributes = vim.loop.fs_stat(p)
+    if attributes and attributes.type == "directory" then
+      error("Expected file path but got directory: " .. p)
+    end
+    local f = io.open(p, "r")
+    if f then
+      local llm_file = LlmFile.new(p, f:read("*all"))
+      files_string = files_string .. "\n\n" .. llm_file:formatted_contents()
+      f:close()
+    end
+  end
+  return files_string
+end
+
 function M.invoke_llm_and_stream_into_editor(opts, make_curl_args_fn, handle_data_fn)
+  local llm_paths = get_llmfile_paths()
   vim.api.nvim_clear_autocmds { group = group }
   local prompt = get_prompt(opts)
-  local system_prompt = opts.system_prompt or 'You are a tsundere uwu anime. Yell at me for not setting my configuration for my llm plugin correctly'
+  local system_prompt = opts.system_prompt or
+  'You are a tsundere uwu anime. Yell at me for not setting my configuration for my llm plugin correctly'
+  system_prompt = system_prompt_with_files(llm_paths, system_prompt)
   local args = make_curl_args_fn(opts, prompt, system_prompt)
   local curr_event_state = nil
 
